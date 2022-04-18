c9645e811be5c13e44362d243823aa4635f40835
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2022-04-18 16:43:31 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.header__burger').click(function () {
+        $(this).toggleClass('open');
+        $('.header__nav').toggleClass('show');
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+});
