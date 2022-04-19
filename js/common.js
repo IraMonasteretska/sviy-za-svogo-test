ca8b585ed2fe2ea542f0c2f81cdbb530c55baf83
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2022-04-19 15:52:32 +0300

    js

--- js/common.js
+++ js/common.js
@@ -8,8 +8,37 @@ $(document).ready(function () {
 
 
 
+    $('.copytext').click(function () {
+
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val($(this).text()).select();
+        document.execCommand("copy");
+        $temp.remove();
+
+    });
 
 
+    $(".scrolllink").on("click", 'a', function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top - 100;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({
+            scrollTop: top
+        }, 1000);
+    });
+
+    $('.scrolllink a').click(function () {
+        $('.header__burger').removeClass('open');
+        $('.header__nav').removeClass('show');
+    });
 
 
 
